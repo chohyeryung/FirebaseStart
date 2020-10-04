592ecfab1df6a7cef73c7d7ff254eae5e6b813a5
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-10-04 14:55:47 +0900

    실시간 디비로 메모 리스트 구현!!

--- app/src/main/java/kr/hs/emirim/cho/firebasestart/realtimedb/MemoActivity.java
+++ app/src/main/java/kr/hs/emirim/cho/firebasestart/realtimedb/MemoActivity.java
@@ -1,16 +1,26 @@
 package kr.hs.emirim.cho.firebasestart.realtimedb;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 import kr.hs.emirim.cho.firebasestart.R;
 
@@ -18,6 +28,9 @@ public class MemoActivity extends AppCompatActivity implements View.OnClickListe
 
     private ArrayList<MemoItem> memoItems = null;
     private MemoAdapter memoAdapter = null;
+    private String username=null;
+    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference=firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,13 @@ public class MemoActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        addChildEvent();
+    }
+
     private void regMemo() {
         EditText titleedit=(EditText)findViewById(R.id.memotitle);
         EditText contentsedit=(EditText)findViewById(R.id.memocontents);
@@ -46,15 +66,53 @@ public class MemoActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
         MemoItem item=new MemoItem();
+        item.setUser(this.username);
         item.setMemotitle(titleedit.getText().toString());
         item.setMemocontents(contentsedit.getText().toString());
 
-        memoItems.add(item);
-        memoAdapter.notifyDataSetChanged();
+        databaseReference.child("memo").push().setValue(item);
+
+//        memoItems.add(item);
+//        memoAdapter.notifyDataSetChanged();
+    }
+
+    private void addChildEvent(){
+        databaseReference.child("memo").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.d("cholingling", "addChildEvent in");
+                MemoItem item=snapshot.getValue(MemoItem.class);
+
+                memoItems.add(item);
+                memoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void init(){
         memoItems=new ArrayList<>();
+
+        username="user_"+new Random().nextInt(1000);
     }
 
     private void initView(){
