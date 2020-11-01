0084ce129b3fcdd9d7783886c5ff7b84da909934
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-01 15:44:59 +0900

    파이어베이스 파이어스토어 액티비티 생성

---
+++ app/src/main/java/kr/hs/emirim/cho/firebasestart/cloudstorage/DownloadActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.cho.firebasestart.cloudstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.cho.firebasestart.R;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
