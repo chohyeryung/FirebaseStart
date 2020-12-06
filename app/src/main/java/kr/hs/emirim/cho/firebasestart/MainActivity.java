08d0d5d737b305bc7c7d72d4f457d31e9b3b6e46
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-12-06 14:24:56 +0900

    intent commit

--- app/src/main/java/kr/hs/emirim/cho/firebasestart/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cho/firebasestart/MainActivity.java
@@ -15,6 +15,7 @@ import kr.hs.emirim.cho.firebasestart.cloudstorage.CloudStorageActivity;
 import kr.hs.emirim.cho.firebasestart.cloudstorage.UploadActivity;
 import kr.hs.emirim.cho.firebasestart.firestore.FirestoreActivity;
 import kr.hs.emirim.cho.firebasestart.hosting.HostingActivity;
+import kr.hs.emirim.cho.firebasestart.performance.PerformanceActivity;
 import kr.hs.emirim.cho.firebasestart.realtimedb.MemoActivity;
 
 
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button firebasehostingebtn=(Button)findViewById(R.id.firebasehostingebtn);
         firebasehostingebtn.setOnClickListener(this);
 
+        Button firebaseperformancebtn=(Button)findViewById(R.id.firebaseperformancebtn);
+        firebaseperformancebtn.setOnClickListener(this);
+
         FirebaseCrashlytics.getInstance().log("안녕하세요~ 크래쉬리틱스님~");
 
     }
@@ -48,28 +52,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         //Toast.makeText(this,"버튼 클릭", Toast.LENGTH_SHORT).show();
         Log.d("파베: Main", "파베어스 버튼 눌림!");
-
+        Intent intent;
         switch (view.getId()) {
             case R.id.firebaseauthbtn:
-                Intent i = new Intent(this, AuthActivity.class);
-                startActivity(i);
+                intent = new Intent(this, AuthActivity.class);
+                startActivity(intent);
                 break;
             case R.id.firebaserealtimedbbtn :
-                Intent i2 = new Intent(this, MemoActivity.class);
-                startActivity(i2);
+                intent = new Intent(this, MemoActivity.class);
+                startActivity(intent);
                 break;
             case R.id.firebasefirestorebtn:
-                Intent i3=new Intent(this, FirestoreActivity.class);
-                startActivity(i3);
+                intent=new Intent(this, FirestoreActivity.class);
+                startActivity(intent);
                 break;
             case R.id.firebasestoragebtn:
-                Intent i4=new Intent(this, CloudStorageActivity.class);
-                startActivity(i4);
+                intent=new Intent(this, CloudStorageActivity.class);
+                startActivity(intent);
                 break;
             case R.id.firebasehostingebtn:
                 Log.d("MainActivity====>", "파이어베이스 클라우드호스팅 버튼 눌림!");
-                Intent i5=new Intent(this, HostingActivity.class);
-                startActivity(i5);
+                intent=new Intent(this, HostingActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.firebaseperformancebtn:
+                Log.d("MainActivity====>", "파이어베이스 퍼포먼스 버튼 눌림!");
+                intent=new Intent(this, PerformanceActivity.class);
+                startActivity(intent);
                 break;
             default:
                 break;
