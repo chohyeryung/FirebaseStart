180178fab21460454480257c69a93b790d77f0f7
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-12-06 14:22:31 +0900

    performance 모니터링 commit 1

---
+++ app/src/main/java/kr/hs/emirim/cho/firebasestart/performance/PerformanceActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.cho.firebasestart.performance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.emirim.cho.firebasestart.R;
+
+public class PerformanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_performance);
+    }
+}
